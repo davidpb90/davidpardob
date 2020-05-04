fec3c540fdf24f41dec71efefbb84fa6877c2b63
David Pardo <davidpb90@gmail.com>
2020-05-04 23:57:51 +0200

    trial with a different normal distribution to generate noise

--- content/post/first-art/paintings.R
+++ content/post/first-art/paintings.R
@@ -29,8 +29,8 @@ for (n in 1:ndrops){
     for (j in 1:pixels){
       #d <- sqrt((i-x)^2+(j-y)^2)
       d <- max(sqrt((i-x)^2+(j-y)^2),0.01) 
-      if (d < r) A[i,j] <- A[i,j]+rnorm(1, mean = d, sd = d/2)
-      #if (d < r) A[i,j] <- A[i,j]+rnorm(1, mean = 1/max(d,0.01), sd = d/2)
+      #if (d < r) A[i,j] <- A[i,j]+rnorm(1, mean = d, sd = d/2)
+      if (d < r) A[i,j] <- A[i,j]+rnorm(1, mean = 1/max(d,0.01), sd = d/2)
       #if (d < r) A[i,j] <- A[i,j]+rt(1,df = d)
     }
   }
@@ -63,7 +63,7 @@ ggplot(df, aes(x, y, color = c)) +
         panel.border = element_rect(color="black", fill = NA)) -> plot
 
 # Do you like it? Save it!
-ggsave(here('content/post/first-art/beta_trial.png'), plot, height =  6, width =  6)
+ggsave(here('content/post/first-art/inverted_normal_trial.png'), plot, height =  6, width =  6)
 
 
 
